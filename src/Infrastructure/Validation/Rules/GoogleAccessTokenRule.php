8e593c2aa9c11fc990e470932613c6860915aff1
shahinyanm <shahinyanm@gmail.com>
2024-07-07 19:51:53 +0400

    FIx php stan  and change larastan package

---
+++ src/Infrastructure/Validation/Rules/GoogleAccessTokenRule.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Infrastructure\Validation\Rules;
+
+
+use Exception;
+use Google_Client;
+use Google_Service_Oauth2;
+use Illuminate\Contracts\Validation\Rule;
+
+class GoogleAccessTokenRule implements Rule
+{
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The provided Google access token is invalid.';
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param string $attribute
+     * @param mixed  $value
+     *
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $client = new Google_Client(['client_id' => env('GOOGLE_CLIENT_ID')]);
+
+        try {
+            $client->setAccessToken($value);
+            $oauth2   = new Google_Service_Oauth2($client);
+            $userInfo = $oauth2->userinfo->get();
+
+            return !empty($userInfo);
+        } catch(Exception $e) {
+            return false;
+        }
+    }
+}
